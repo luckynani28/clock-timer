2ee0cd180f1326eb76553070b50c521d2fb7e8ed
luckynani28 <venkatalakshmikande@gmail.com>
2018-11-08 10:12:12 +0530

    clock commit

---
+++ src/app/clock/clock.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.css']
+})
+export class ClockComponent implements OnInit {
+d: any;
+
+  constructor() { }
+  ngOnInit() {
+   setInterval(() => {
+      this.d = new Date();
+  }, 1000 );
+
+  }
+
+}
